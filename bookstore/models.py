088606055e9882bdb2825eaf538a39a565d1e3c2
Sivar-Developer <fragmatica.dc@gmail.com>
2022-02-03 11:00:08 +0300

    Migration

--- bookstore/models.py
+++ bookstore/models.py
@@ -1,3 +1,17 @@
+import email
+from unicodedata import name
 from django.db import models
 
 # Create your models here.
+
+
+class Customer(models.Model):
+    name = models.CharField(max_length=190, null=True)
+    email = models.EmailField()
+    phone = models.CharField(max_length=190, null=True)
+    age = models.CharField(max_length=190, null=True)
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+    
+    def __str__(self):
+        return self.name
+    

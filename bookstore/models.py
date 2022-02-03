419404e5b10badb9acc5427d56852bb12afb750e
Sivar-Developer <fragmatica.dc@gmail.com>
2022-02-03 11:37:57 +0300

    Models relationships, migrations

--- bookstore/models.py
+++ bookstore/models.py
@@ -15,3 +15,41 @@ class Customer(models.Model):
     def __str__(self):
         return self.name
     
+class Tag(models.Model):
+    name = models.CharField(max_length=190, null=True)
+    
+    def __str__(self):
+        return self.name
+
+class Book(models.Model):
+    CATEGORY = (
+        ('Classics','Classics'),
+        ('Comic','Comic'),
+        ('Fantasy','Fantasy'),
+        ('Horror','Horror'),
+    )
+    
+    name = models.CharField(max_length=190, null=True)
+    author = models.CharField(max_length=190, null=True)
+    price = models.FloatField(null=True)
+    category = models.CharField(max_length=190, null=True, choices=CATEGORY)
+    description = models.CharField(max_length=190, null=True)
+    tags = models.ManyToManyField(Tag)
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+    
+    def __str__(self):
+        return self.name
+    
+class Order(models.Model):
+    STATUS = (
+        ('Pending','Pending'),
+        ('Delivered','Delivered'),
+        ('In progress','In progress'),
+        ('Out of order','Out of order'),
+    )
+    
+    customer = models.ForeignKey(Customer, null=True, on_delete=models.SET_NULL)
+    book = models.ForeignKey(Book, null=True, on_delete=models.SET_NULL)
+    tags = models.ManyToManyField(Tag)
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+    status = models.CharField(max_length=200, null=True, choices=STATUS)

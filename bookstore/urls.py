1aaa31028fdaccbc08031e792a5d69d7c45af772
Sivar-Developer <fragmatica.dc@gmail.com>
2022-02-21 16:16:01 +0300

    URL naming

--- bookstore/urls.py
+++ bookstore/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('home', views.home),
-    path('books', views.books),
+    path('home', views.home, name="home"),
+    path('books', views.books, name="books"),
     # path('customer', views.customer),
-    path('customer/<str:pk>', views.customer),
+    path('customer/<str:pk>', views.customer, name="customer"),
 ]
